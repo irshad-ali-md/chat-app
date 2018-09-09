99556f29a2a18d789a270f73e6c8ca3ddae8bc09
irshad-ali-md <irshadali6541@gmail.com>
2018-09-09 21:04:51 +0530

    adding chat app

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/6a5878bc-c5f1-4e4c-ab8b-e5f269881866/token";
+
+const instanceLocator = "v1:us1:6a5878bc-c5f1-4e4c-ab8b-e5f269881866";
+
+export { tokenUrl, instanceLocator };
